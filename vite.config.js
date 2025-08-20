0bd3536a01d26adb3b26120a782609ded6b58242
chelseapaulhus-willow <chelseapaulhus@gmail.com>
2025-08-20 13:37:54 -0400

    Iphone record button working

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./.cert/dev-key.pem'),
+      cert: fs.readFileSync('./.cert/dev-cert.pem'),
+    },
+    hmr: {
+      host: '192.168.1.174',   // same IP as above
+      protocol: 'wss',
+      port: 5173,
+    },
+  },
+})
